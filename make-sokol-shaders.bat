55822ca69dab8c69e56c69e72e303b468e8f7496
adambigg-s <biggsa601@gmail.com>
2025-05-15 18:11:57 -0400

    shader

--- make-sokol-shaders.bat
+++ make-sokol-shaders.bat
@@ -3,7 +3,7 @@
 set arg1=%1
 if "%arg1%"=="" set arg1=clean
 
-sokol-shdc.exe -i ./src/shaders.glsl -o ./src/shaders.h --slang hlsl5:wgsl:glsl430
+sokol-shdc.exe -i ./src/shaders.glsl -o ./src/shaders.hpp --slang hlsl5:wgsl:glsl430
 
 make %arg1%
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,29 @@
 #include "../sokol/sokol_gfx.h"
 #include "../sokol/sokol_glue.h"
 #include "../sokol/sokol_log.h"
-#include "shaders.h"
+#include "shaders.hpp"
 
 #define WIDTH 800
 #define HEIGHT 600
 
 constexpr sg_color BACKGROUND_COLOR = sg_color{.r = 0.2, .g = 0.2, .b = 0.3};
 
-static struct {
+typedef struct State {
     sg_pass_action pass_action;
     sg_bindings bindings;
     sg_pipeline pipeline;
-} state;
+} State;
 
-void init() {
+void init(void *state_ptr) {
+    State *state = (State *)state_ptr;
+
     sg_desc description = sg_desc{};
     description.environment = sglue_environment();
     description.logger = sg_logger{.func = slog_func};
     sg_setup(&description);
 
     sg_shader shader = sg_make_shader(simple_shader_desc(sg_query_backend()));
+
     // clang-format off
     float vertices[] = {
         -0.5, -0.5, 0.0,
@@ -32,60 +35,73 @@ void init() {
         0.0, 0.5, 0.0,
     };
     // clang-format on
+    sg_buffer_desc buffer_desc = {};
+    buffer_desc.size = sizeof(vertices);
+    buffer_desc.data = sg_range{.ptr = &vertices, .size = sizeof(vertices)};
+    buffer_desc.label = "triangle vertices";
+    state->bindings.vertex_buffers[0] = sg_make_buffer(&buffer_desc);
 
-    sg_buffer_desc buffer = {};
-    buffer.size = sizeof(vertices);
-    buffer.data = SG_RANGE(vertices);
-    buffer.label = "triangle vertices";
-    state.bindings.vertex_buffers[0] = sg_make_buffer(&buffer);
-
-    sg_pipeline_desc pipeline = sg_pipeline_desc{};
-    pipeline.shader = shader;
-    pipeline.layout.attrs[ATTR_simple_position].format = SG_VERTEXFORMAT_FLOAT3;
-    pipeline.label = "triangle pipeline";
-    state.pipeline = sg_make_pipeline(pipeline);
+    sg_pipeline_desc pipe_desc = sg_pipeline_desc{};
+    pipe_desc.shader = shader;
+    pipe_desc.layout.attrs[ATTR_simple_position].format = SG_VERTEXFORMAT_FLOAT3;
+    pipe_desc.label = "triangle pipeline";
+    state->pipeline = sg_make_pipeline(pipe_desc);
 
     sg_pass_action pass_action = sg_pass_action{};
     pass_action.colors[0] =
         sg_color_attachment_action{.load_action = SG_LOADACTION_CLEAR, .clear_value = BACKGROUND_COLOR};
-    state.pass_action = pass_action;
+    state->pass_action = pass_action;
 }
 
-void frame() {
-    sg_pass pass_action = sg_pass{};
-    pass_action.action = state.pass_action;
-    pass_action.swapchain = sglue_swapchain();
-    sg_begin_pass(&pass_action);
+void frame(void *state_ptr) {
+    State *state = (State *)state_ptr;
 
-    sg_apply_pipeline(state.pipeline);
-    sg_apply_bindings(&state.bindings);
+    sg_pass pass = sg_pass{};
+    pass.action = state->pass_action;
+    pass.swapchain = sglue_swapchain();
+    sg_begin_pass(&pass);
+
+    sg_apply_pipeline(state->pipeline);
+    sg_apply_bindings(&state->bindings);
     sg_draw(0, 3, 1);
 
     sg_end_pass();
     sg_commit();
 }
 
-void event(const sapp_event *event) {
+void event(const sapp_event *event, void *state_ptr) {
+    State *state = (State *)state_ptr;
+
     if (event->type == SAPP_EVENTTYPE_KEY_DOWN && event->key_code == SAPP_KEYCODE_ESCAPE) {
         sapp_request_quit();
     }
 }
 
-void cleanup() {
+void cleanup(void *user_data) {
+    State *state = (State *)user_data;
+
     sg_shutdown();
+    free(user_data);
 }
 
-sapp_desc sokol_main(int argc, char *argv[]) {
+sapp_desc sokol_main(int _argc, char *_argv[]) {
+    State *state_ptr = (State *)malloc(sizeof(State));
+    *state_ptr = State{};
+
     sapp_desc description = sapp_desc{};
-    description.init_cb = init;
-    description.frame_cb = frame;
-    description.event_cb = event;
-    description.cleanup_cb = cleanup;
+    description.user_data = state_ptr;
+    description.init_userdata_cb = init;
+    description.frame_userdata_cb = frame;
+    description.event_userdata_cb = event;
+    description.cleanup_userdata_cb = cleanup;
     description.width = WIDTH;
     description.height = HEIGHT;
     description.high_dpi = true;
     description.window_title = "boids simulation with Sokol";
     description.icon = sapp_icon_desc{.sokol_default = true};
+    description.fullscreen = false;
+    description.sample_count = 4;
+    description.logger = sapp_logger{.func = slog_func};
 
     return description;
 }
